79783868cd185e08ca11373457a96a240291c7cc
Ashutosh Mishra <ashutosh.mishra@araymond.com>
2025-01-03 01:48:35 +0100

    chore(misc): rewrite configs, wip

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,168 +1,94 @@
-
 local on_attach = require("util.lsp").on_attach
-local diagnostic_signs = require("util.icons").diagnostic_signs
-
+local icons = require("util.icons").diagnostics
+local servers_lsp = require("plugins.lsp.servers")
 
 local config = function()
-	require("neoconf").setup({})
-	local lsp = require("blink.cmp")
-	local lspconfig = require("lspconfig")
-	local capabilities = lsp.get_lsp_capabilities()
+  require("neoconf").setup({})
+  local lsp = require("blink.cmp")
+  local lspconfig = require("lspconfig")
+  local capabilities = lsp.get_lsp_capabilities()
 
+  local signs = { Error = icons.Error, Warn = icons.Warning, Hint = icons.Hint, Info = icons.Information }
+  for type, icon in pairs(signs) do
+    local hl = 'DiagnosticSign' .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+  end
 
-  -- lua
-	lspconfig.lua_ls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		settings = { -- custom settings for lua
-			Lua = {
-				-- make the language server recognize "vim" global
-				diagnostics = {
-					globals = { "vim" },
-				},
-        format = {
-                            enable = true,
-                            defaultConfig = {
-                                align_continuous_assign_statement = false,
-                                align_continuous_rect_table_field = false,
-                                align_array_table = false,
-                            },
-                        },
-				workspace = {
-					library = {
-						vim.fn.expand("$VIMRUNTIME/lua"),
-						vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
-					},
-				},
-			},
-		},
-	})
+  for server, opts in pairs(servers_lsp) do
+    opts.capabilities = capabilities
+    opts.on_attach = on_attach
 
-  -- json
-	lspconfig.jsonls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		filetypes = { "json", "jsonc" },
-	})
+    ---@diagnostic disable-next-line: missing-fields
+    lspconfig[server].setup(opts)
+  end
 
-	-- python
-	lspconfig.pyright.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		settings = {
-			-- pyright = {
-			-- 	disableOrganizeImports = false,
-			-- 	analysis = {
-			-- 		useLibraryCodeForTypes = true,
-			-- 		autoSearchPaths = true,
-			-- 		diagnosticMode = "workspace",
-			-- 		autoImportCompletions = true,
-			-- 	},
-			-- },
-		},
-	})
-
-
-	-- bash
-	lspconfig.bashls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		filetypes = { "sh", "aliasrc" },
-	})
-
-	-- docker
-	lspconfig.dockerls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-	})
-
-	-- C/C++
-	lspconfig.clangd.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		cmd = {
-			"clangd",
-			"--offset-encoding=utf-16",
-		},
-	})
-
-	-- for type, icon in pairs(diagnostic_signs) do
-	-- 	local hl = "DiagnosticSign" .. type
-	-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-	-- end
-
-	local solhint = require("efmls-configs.linters.solhint")
-	local prettier_d = require("efmls-configs.formatters.prettier_d")
-	local luacheck = require("efmls-configs.linters.luacheck")
-	local stylua = require("efmls-configs.formatters.stylua")
-	local flake8 = require("efmls-configs.linters.flake8")
-	local black = require("efmls-configs.formatters.black")
-	local eslint = require("efmls-configs.linters.eslint")
-	local fixjson = require("efmls-configs.formatters.fixjson")
-	local shellcheck = require("efmls-configs.linters.shellcheck")
-	local shfmt = require("efmls-configs.formatters.shfmt")
-	local hadolint = require("efmls-configs.linters.hadolint")
-	local cpplint = require("efmls-configs.linters.cpplint")
-	local clangformat = require("efmls-configs.formatters.clang_format")
+  local prettier_d = require("efmls-configs.formatters.prettier_d")
+  local luacheck = require("efmls-configs.linters.luacheck")
+  local stylua = require("efmls-configs.formatters.stylua")
+  local eslint = require("efmls-configs.linters.eslint")
+  local fixjson = require("efmls-configs.formatters.fixjson")
+  local shellcheck = require("efmls-configs.linters.shellcheck")
+  local shfmt = require("efmls-configs.formatters.shfmt")
+  local hadolint = require("efmls-configs.linters.hadolint")
+  local clangformat = require("efmls-configs.formatters.clang_format")
   local ruff_sort = require('efmls-configs.formatters.ruff_sort')
   local ruff = require('efmls-configs.formatters.ruff')
   local rufflint = require('efmls-configs.linters.ruff')
   local clang_tidy = require('efmls-configs.linters.clang_tidy')
 
-	-- configure efm server
-	lspconfig.efm.setup({
-		filetypes = {
-			"lua",
-			"python",
-			"json",
-			"jsonc",
-			"sh",
-			"markdown",
-			"docker",
-			"c",
-			"cpp",
-		},
-		init_options = {
-			documentFormatting = true,
-			documentRangeFormatting = true,
-			hover = true,
-			documentSymbol = true,
-			codeAction = true,
-			completion = true,
-		},
-		settings = {
-			languages = {
-				lua = { luacheck, stylua },
-				python = {ruff, ruff_sort, rufflint },
-				json = { eslint, fixjson },
-				jsonc = { eslint, fixjson },
-				sh = { shellcheck, shfmt },
-				markdown = { prettier_d },
-				docker = { hadolint, prettier_d },
-				c = { clangformat, clnag_tidy },
-				cpp = { clangformat, clnag_tidy },
-			},
-		},
-	})
+  -- configure efm server
+  lspconfig.efm.setup({
+    filetypes = {
+      "lua",
+      "python",
+      "json",
+      "jsonc",
+      "sh",
+      "markdown",
+      "docker",
+      "c",
+      "cpp",
+    },
+    init_options = {
+      documentFormatting = true,
+      documentRangeFormatting = true,
+      hover = true,
+      documentSymbol = true,
+      codeAction = true,
+      completion = true,
+    },
+    settings = {
+      languages = {
+        lua = { luacheck, stylua },
+        python = { ruff, ruff_sort, rufflint },
+        json = { eslint, fixjson },
+        jsonc = { eslint, fixjson },
+        sh = { shellcheck, shfmt },
+        markdown = { prettier_d },
+        docker = { hadolint, prettier_d },
+        c = { clang_tidy, clangformat },
+        cpp = { clang_tidy, clangformat },
+      },
+    },
+  })
 end
 
 return {
 
-    "neovim/nvim-lspconfig",
+  "neovim/nvim-lspconfig",
   config = config,
-	lazy = false,
-    dependencies = {
+  lazy = false,
+  dependencies = {
     "windwp/nvim-autopairs",
-		"creativenull/efmls-configs-nvim",
-		"hrsh7th/cmp-buffer",
+    "creativenull/efmls-configs-nvim",
     "saghen/blink.cmp",
-      {
-        "folke/lazydev.nvim",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
+    {
+      "folke/lazydev.nvim",
+      opts = {
+        library = {
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
       },
     },
+  },
 }
